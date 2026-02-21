2a440d834b051098f539128646cc311267cf81ad
Ryan Williams <roug10.rw@gmail.com>
2026-02-20 21:37:58 -0500

    Queue.h file code has been added.

--- queue.h
+++ queue.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <string>
+
+struct User {
+	std::string username;
+	int level;
+	std:: string faction
+};
+
+struct Node{
+	User data;
+	Node* next;
+};
+
+void initQueue(Queue& q);
+bool isEmpty(Queue& q);
+int enqueue(Queue& q, User u);
+int dequeue(Queue& q, User& u);
+
+int endqueueRandonUsers(Queue& q, int count);
+
+#endif
